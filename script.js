2bb25ee272e4d76f4e88cc0801efdafcf8734d2c
Zen <amaar5ali@gmail.com>
2023-10-28 01:37:51 -0400

    going to make a new slideshow feature for home page after multiple failures on implementing through bugs and breaking the other parts of the app, so going to make home page's own modal and slideshow. Currently, I added a check to have the current modal and slideshow not work for the about me and home pages.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let currentImageIndex = 0;
 let images;
+let isHomePage = false; // Step 1: Add the global flag
 
 document.addEventListener('DOMContentLoaded', function() {
     const links = document.querySelectorAll('#left-sidebar a');
@@ -18,13 +19,17 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function nextImage() {
-        currentImageIndex = (currentImageIndex + 1) % images.length;
-        modalImage.src = images[currentImageIndex].src;
+        if (!isHomePage) {  // Step 2: Check the flag
+            currentImageIndex = (currentImageIndex + 1) % images.length;
+            modalImage.src = images[currentImageIndex].src;
+        }
     }
 
     function prevImage() {
-        currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
-        modalImage.src = images[currentImageIndex].src;
+        if (!isHomePage) {  // Step 2: Check the flag
+            currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+            modalImage.src = images[currentImageIndex].src;
+        }
     }
 
     document.addEventListener('keydown', function(event) {
@@ -37,17 +42,35 @@ document.addEventListener('DOMContentLoaded', function() {
     modal.addEventListener('click', closeModal);
 
     function loadPage(page) {
+        isHomePage = (page === 'home'); // Step 1: Set the flag
+
         const allPages = document.querySelectorAll('.page');
         allPages.forEach(p => p.style.display = 'none');
+
         const selectedPage = document.getElementById(`page-${page}`);
         selectedPage.style.display = 'block';
-        images = document.querySelectorAll(`#page-${page} img`);
-        images.forEach((img, index) => {
-            img.addEventListener('click', function() {
-                openModal(this.src, index, page);
-            });
-        });
 
+        images = document.querySelectorAll(`#page-${page} img`);
+
+        // Remove existing click listeners to prevent duplication
+        images.forEach((img) => {
+            const newImg = img.cloneNode(true);
+            img.parentNode.replaceChild(newImg, img);
+        });
+    
+        // Re-query to get the new nodes
+        images = document.querySelectorAll(`#page-${page} img`);
+
+        // Conditionally attach click listeners, skip for 'home' and 'about'
+        if (page !== 'home' && page !== 'about') {
+            images.forEach((img, index) => {
+                img.addEventListener('click', function() {
+                    openModal(this.src, index, page);
+                });
+            });
+        }
+    
+        // Existing 18+ check for 'boudoir'
         if (page === 'boudoir') {
             const is18 = confirm("You must be 18+ to view this content. Are you 18 or older?");
             if (!is18) {
